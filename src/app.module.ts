cd28e35e57d01953145a6c82c986ac23a1fd9021
soubhagya <soubhagya.r.nayak@gmail.com>
2019-05-31 21:38:41 +0530

    added config support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config.module';
+import { HnService } from './hn/hn.service'
+import { HnController } from './hn/hn.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule],
+  controllers: [AppController, HnController],
+  providers: [AppService, HnService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { config } from 'dotenv'
 
 async function bootstrap() {
+  config({path: join(__dirname,'..',  '.env')});
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useStaticAssets(join(__dirname,'..','/node_modules/bootstrap/dist'));
