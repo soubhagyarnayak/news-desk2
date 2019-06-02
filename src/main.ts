bf496805b2310b681e14b5d3bb61b99259ae4607
soubhagya <soubhagya.r.nayak@gmail.com>
2019-06-03 01:01:57 +0530

    added morgan for logging requests

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { config } from 'dotenv'
+import { config } from 'dotenv';
+import morgan = require('morgan');
 
 async function bootstrap() {
   config({path: join(__dirname,'..',  '.env')});
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(morgan('dev'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useStaticAssets(join(__dirname,'..','/node_modules/bootstrap/dist'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
